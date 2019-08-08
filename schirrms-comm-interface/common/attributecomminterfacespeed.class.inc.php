60fc56193afc7dd57cc83661088f24b7fe3ead28
Schirrms <schirrms@schirrms.net>
2019-08-08 18:39:42 +0200

    Glitch

--- schirrms-comm-interface/common/attributecomminterfacespeed.class.inc.php
+++ schirrms-comm-interface/common/attributecomminterfacespeed.class.inc.php
@@ -11,6 +11,33 @@ class AttributeCommInterfaceSpeed extends AttributeDecimal
 	/**
 	 * @inheritdoc
 	 */
+	public function GetLabel($sValue)
+	{
+		$sValueLabel = parent::GetValueLabel($sValue);
+		if(($sValueLabel !== null) && ($sValueLabel !== ''))
+		{
+			$iLevel = 15;
+			$sUnit = ' KMGTE';
+			for ($iLevel=15; $iLevel >0 ;$iLevel=$iLevel-3)
+			{
+				if ($sValueLabel >= 10**$iLevel)
+				{
+					$sValueLabel = sprintf('%.3g ', $sValueLabel / (10**$iLevel)) . substr($sUnit, $iLevel/3, 1);
+					break;
+				}
+			}
+		}
+		else
+		{
+			$sValueLabel = '-';
+		}
+
+		return $sValueLabel;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
 	public function GetValueLabel($sValue)
 	{
 		$sValueLabel = parent::GetValueLabel($sValue);
