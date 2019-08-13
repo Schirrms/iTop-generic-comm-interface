e46fbcfc984676a69c351b0d942a7ea79fa7760a
Schirrms <schirrms@schirrms.net>
2019-08-13 17:01:14 +0200

    Add redundancy (0.3.4.0)

--- schirrms-comm-interface/module.schirrms-comm-interface.php
+++ schirrms-comm-interface/module.schirrms-comm-interface.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'schirrms-comm-interface/0.3.3',
+	'schirrms-comm-interface/0.3.4',
 	array(
 		// Identification
 		//
