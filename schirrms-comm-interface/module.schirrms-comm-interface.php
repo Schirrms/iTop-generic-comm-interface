d95eeee042a25970db5b15dcb7741c4829910655
Schirrms <schirrms@schirrms.net>
2019-08-26 15:11:09 +0200

    Correct extension of the class Model -> Field type (version 0.5.5.0)

--- schirrms-comm-interface/module.schirrms-comm-interface.php
+++ schirrms-comm-interface/module.schirrms-comm-interface.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'schirrms-comm-interface/0.5.4',
+	'schirrms-comm-interface/0.5.5',
 	array(
 		// Identification
 		//
